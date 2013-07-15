87d966806f8e1471c4369d499627955a8dce3940
bsitu <bsitu@wikimedia.org>
2013-07-14 21:15:53 -0700

    (bug 51303) Do not send duplicate thanks notification
    Change-Id: Ie3c31e4ce155541a6aa9b6feae433742be2967e8

--- ApiThank.php
+++ ApiThank.php
@@ -29,46 +29,51 @@ class ApiThank extends ApiBase {
 		if ( !$rev ) {
 			$this->dieUsage( 'Revision ID is not valid', 'invalidrevision' );
 		} else {
-			$title = Title::newFromID( $rev->getPage() );
-			if ( !$title ) {
-				$this->dieUsage( 'Page title could not be retrieved', 'notitle' );
-			}
+			// Do not send notification if session data says "it has already been sent"
+			if ( !$agent->getRequest()->getSessionData( "thanks-thanked-{$rev->getId()}" ) ) {
+				$title = Title::newFromID( $rev->getPage() );
+				if ( !$title ) {
+					$this->dieUsage( 'Page title could not be retrieved', 'notitle' );
+				}
 
-			// Get the user ID of the user who performed the edit
-			$recipient = $rev->getUser();
+				// Get the user ID of the user who performed the edit
+				$recipient = $rev->getUser();
 
-			if ( !$recipient ) {
-				$this->dieUsage( 'No valid recipient found', 'invalidrecipient' );
-			} else {
-				// Set the source of the thanks, e.g. 'diff' or 'history'
-				if ( $params['source'] ) {
-					$source = trim( $params['source'] );
+				if ( !$recipient ) {
+					$this->dieUsage( 'No valid recipient found', 'invalidrecipient' );
 				} else {
-					$source = 'undefined';
+					// Set the source of the thanks, e.g. 'diff' or 'history'
+					if ( $params['source'] ) {
+						$source = trim( $params['source'] );
+					} else {
+						$source = 'undefined';
+					}
+					// Create the notification via Echo extension
+					EchoEvent::create( array(
+						'type' => 'edit-thank',
+						'title' => $title,
+						'extra' => array(
+							'revid' => $rev->getId(),
+							'thanked-user-id' => $recipient,
+							'source' => $source,
+						),
+						'agent' => $agent,
+					) );
+					// Mark the thank in session to prevent duplicates (Bug 46690)
+					$agent->getRequest()->setSessionData( "thanks-thanked-{$rev->getId()}", true );
+					// Set success message
+					$result['success'] = '1';
+					// Log it if we're supposed to log it
+					if ( $wgThanksLogging ) {
+						$logEntry = new ManualLogEntry( 'thanks', 'thank' );
+						$logEntry->setPerformer( $agent );
+						$target = User::newFromId( $recipient )->getUserPage();
+						$logEntry->setTarget( $target );
+						$logid = $logEntry->insert();
+					}
 				}
-				// Create the notification via Echo extension
-				EchoEvent::create( array(
-					'type' => 'edit-thank',
-					'title' => $title,
-					'extra' => array(
-						'revid' => $rev->getId(),
-						'thanked-user-id' => $recipient,
-						'source' => $source,
-					),
-					'agent' => $agent,
-				) );
-				// Mark the thank in session to prevent duplicates (Bug 46690)
-				$agent->getRequest()->setSessionData( "thanks-thanked-{$rev->getId()}", true );
-				// Set success message
+			} else {
 				$result['success'] = '1';
-				// Log it if we're supposed to log it
-				if ( $wgThanksLogging ) {
-					$logEntry = new ManualLogEntry( 'thanks', 'thank' );
-					$logEntry->setPerformer( $agent );
-					$target = User::newFromId( $recipient )->getUserPage();
-					$logEntry->setTarget( $target );
-					$logid = $logEntry->insert();
-				}
 			}
 		}
 		$this->getResult()->addValue( null, 'result', $result );
