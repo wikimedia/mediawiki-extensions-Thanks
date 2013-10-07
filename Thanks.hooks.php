05c7f89a8e5a43e33913fca3d963b78ffe637710
Kunal Mehta <legoktm@gmail.com>
2013-10-07 13:46:48 -0700

    Only allow Thanks for public revisions
    Bug: 54100
    Change-Id: Ied2846076ecf86b7045563fe5f2b0c002bd42647

--- Thanks.hooks.php
+++ Thanks.hooks.php
@@ -25,6 +25,7 @@ class ThanksHooks {
 			&& !$wgUser->isAnon()
 			&& $rev->getUser() !== $wgUser->getId()
 			&& !$wgUser->isBlocked()
+			&& !$rev->isDeleted( Revision::DELETED_TEXT )
 		) {
 			$recipient = User::newFromId( $rev->getUser() );
 			$recipientAllowed = true;
