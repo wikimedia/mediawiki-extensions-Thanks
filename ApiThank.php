0172b6eb58721d730e7b4d587109f62875d1fc61
Kunal Mehta <legoktm@gmail.com>
2013-12-05 23:12:32 -0800

    Send Thanks log entries to UDP
    Change-Id: I57ff962f1f53b2236265381132c396fcd7ce8e20

--- ApiThank.php
+++ ApiThank.php
@@ -126,7 +126,8 @@ class ApiThank extends ApiBase {
 			$logEntry->setPerformer( $user );
 			$target = $recipient->getUserPage();
 			$logEntry->setTarget( $target );
-			$logEntry->insert();
+			$logId = $logEntry->insert();
+			$logEntry->publish( $logId, 'udp' );
 		}
 	}
 
