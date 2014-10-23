121f4932b93182f2cfeac1124edaa2a02c99c322
Kunal Mehta <legoktm@gmail.com>
2014-10-23 09:46:47 -0700

    Register directory with UnitTestsList hook
    Change-Id: I7a6498af48c0d62428bf012d5045ad3db60aa0cc

--- Thanks.hooks.php
+++ Thanks.hooks.php
@@ -268,20 +268,7 @@ class ThanksHooks {
 	 * @return boolean
 	 */
 	public static function registerUnitTests( array &$files ) {
-		// @codeCoverageIgnoreStart
-		$directoryIterator = new RecursiveDirectoryIterator( __DIR__ . '/tests/' );
-
-		/**
-		 * @var SplFileInfo $fileInfo
-		 */
-		$ourFiles = array();
-		foreach ( new RecursiveIteratorIterator( $directoryIterator ) as $fileInfo ) {
-			if ( substr( $fileInfo->getFilename(), -8 ) === 'Test.php' ) {
-				$ourFiles[] = $fileInfo->getPathname();
-			}
-		}
-
-		$files = array_merge( $files, $ourFiles );
+		$files[] = __DIR__ . '/tests/';
 		return true;
 		// @codeCoverageIgnoreEnd
 	}
