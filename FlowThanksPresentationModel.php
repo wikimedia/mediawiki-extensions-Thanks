190ace1ed7f75e4b1d6d036542ec1693fafeb411
Roan Kattouw <roan.kattouw@gmail.com>
2015-10-28 00:23:38 -0700

    Convert Echo notifications to the new system
    * Add ThanksPresentationModel and FlowThanksPresentationModel
    * Rename notification-thanks-flyout2 to notification-header-edit-thank,
      shift parameters around and add GENDER support for thanked user
    * Rename notification-flow-thanks-flyout to notification-header-flow-thank
      and shift parameters around; this one did already have GENDER support
      for the thanked user
    
    Depends on Idb975feaec in Echo.
    
    Bug: T116847
    Change-Id: Iab0f2311e7ffb6a9ce21cd76e564511d03805530

---
+++ FlowThanksPresentationModel.php
@@ -0,0 +1,52 @@
+<?php
+class EchoFlowThanksPresentationModel extends EchoEventPresentationModel {
+	public function canRender() {
+		return (bool)$this->event->getTitle();
+	}
+
+	public function getIconType() {
+		return 'thanks';
+	}
+
+	public function getHeaderMessage() {
+		$msg = parent::getHeaderMessage();
+		$msg->params( $this->event->getExtraParam( 'topic-title' ) );
+		$msg->params( $this->event->getTitle()->getPrefixedText() );
+
+		$user = User::newFromId( $this->event->getExtraParam( 'thanked-user-id' ) );
+		$msg->params( $user ? $user->getName() : '[]' );
+		return $msg;
+	}
+
+	public function getPrimaryLink() {
+		$title = $this->event->getTitle();
+		// Make a link to #flow-post-{postid}
+		$title = Title::makeTitle(
+			$title->getNamespace(),
+			$title->getDBKey(),
+			'flow-post' . $this->event->getExtraParam( 'post-id' )
+		);
+
+		return array(
+			$title->getFullURL( array(
+				'workflow' => $this->event->getExtraParam( 'workflow' )
+			) ),
+			$this->msg( 'notification-link-text-view-post' )
+		);
+	}
+
+	// TODO add thanking user as secondary link once we can make that look nice (T115421)
+	/*
+	public function getSecondaryLinks() {
+		$agent = $this->event->getAgent();
+		if ( !$agent || !$this->userCan( Revision::DELETED_USER ) ) {
+			return array();
+		}
+
+		$url = $agent->getUserPage()->getLocalURL();
+		return array(
+			$url => $agent->getName()
+		);
+	}
+	*/
+}
---
+++ ThanksPresentationModel.php
@@ -0,0 +1,44 @@
+<?php
+class EchoThanksPresentationModel extends EchoEventPresentationModel {
+	public function canRender() {
+		return (bool)$this->event->getTitle();
+	}
+
+	public function getIconType() {
+		return 'thanks';
+	}
+
+	public function getHeaderMessage() {
+		$msg = parent::getHeaderMessage();
+		$msg->params( $this->event->getTitle()->getPrefixedText() );
+
+		$user = User::newFromId( $this->event->getExtraParam( 'thanked-user-id' ) );
+		$msg->params( $user ? $user->getName() : '[]' );
+		return $msg;
+	}
+
+	public function getPrimaryLink() {
+		return array(
+			$this->event->getTitle()->getLocalURL( array(
+				'oldid' => 'prev',
+				'diff' => $this->event->getExtraParam( 'revid' )
+			) ),
+			$this->msg( 'notification-link-text-view-edit' )
+		);
+	}
+
+	// TODO add thanking user as secondary link once we can make that look nice (T115421)
+	/*
+	public function getSecondaryLinks() {
+		$agent = $this->event->getAgent();
+		if ( !$agent || !$this->userCan( Revision::DELETED_USER ) ) {
+			return array();
+		}
+
+		$url = $agent->getUserPage()->getLocalURL();
+		return array(
+			$url => $agent->getName()
+		);
+	}
+	*/
+}
