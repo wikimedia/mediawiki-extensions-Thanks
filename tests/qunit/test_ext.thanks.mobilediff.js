6bf8fd0b1168be64890eae15bdf5c3ce88535ed4
James D. Forrester <jforrester@wikimedia.org>
2017-10-23 13:38:11 -0700

    tests: Make compartible with QUnit 2
    Change-Id: I726d7f75e37a759e1b1f6372710cb06f754fc94b

--- tests/qunit/test_ext.thanks.mobilediff.js
+++ tests/qunit/test_ext.thanks.mobilediff.js
@@ -1,7 +1,7 @@
 ( function ( $, mw ) {
 	QUnit.module( 'Thanks mobilediff' );
 
-	QUnit.test( 'render button for logged in users', 1, function ( assert ) {
+	QUnit.test( 'render button for logged in users', function ( assert ) {
 		var $container = $( '<div>' ),
 			$user = $( '<div>' ).data( 'user-name', 'jon' )
 				.data( 'revision-id', 1 )
