5d6120dd8f9d6cc83f3e12c53b28a9b07352840a
Fomafix <fomafix@googlemail.com>
2017-12-19 19:30:13 +0100

    Start showing warnings for deprecated PHP entry point
    Change-Id: Ie2919c16fbee13cfe33b0f152f7cd086c2e7002f

--- Thanks.php
+++ Thanks.php
@@ -29,10 +29,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Thanks'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['ThanksAlias'] = __DIR__ . '/Thanks.alias.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Thanks extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 } else {
 	die( 'This version of the Thanks extension requires MediaWiki 1.25+' );
 }
