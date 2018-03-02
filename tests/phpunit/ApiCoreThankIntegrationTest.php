3b2397fea99037339facac0e01057c101ea646f8
MaxSem <maxsem.wiki@gmail.com>
2018-03-02 22:59:20 +0000

    Revert "Add support to the Thanks API for thanking log entries"
    This reverts commit b84eedc74e124990caf4299caf9a706030ac7be1.
    
    Bug: T188791
    Change-Id: If55ed6cbb5da34c0034da3cc564c6a8ff82448d2

--- tests/phpunit/ApiCoreThankIntegrationTest.php
+++ tests/phpunit/ApiCoreThankIntegrationTest.php
@@ -19,11 +19,6 @@ class ApiCoreThankIntegrationTest extends ApiTestCase {
 	 */
 	private $revId;
 
-	/**
-	 * @var int The ID of a deletion log entry.
-	 */
-	protected $logId;
-
 	public function setUp() {
 		parent::setUp();
 
@@ -35,13 +30,6 @@ class ApiCoreThankIntegrationTest extends ApiTestCase {
 		/** @var Revision $revision */
 		$revision = $result['revision'];
 		$this->revId = $revision->getId();
-
-		// Create a 2nd page and delete it, so we can thank for the log entry.
-		$pageToDeleteTitle = Title::newFromText( 'Page to delete' );
-		$pageToDelete = WikiPage::factory( $pageToDeleteTitle );
-		$pageToDelete->doEditContent( ContentHandler::makeContent( '', $pageToDeleteTitle ), '' );
-		$deleteStatus = $pageToDelete->doDeleteArticleReal( '' );
-		$this->logId = $deleteStatus->getValue();
 
 		$this->doLogin( 'sysop' );
 		DeferredUpdates::clearPendingUpdates();
@@ -56,59 +44,12 @@ class ApiCoreThankIntegrationTest extends ApiTestCase {
 		] );
 	}
 
-	public function testValidRevRequest() {
+	public function testValidRequest() {
 		list( $result,, ) = $this->doApiRequestWithToken( [
 			'action' => 'thank',
 			'rev' => $this->revId,
 		] );
 		$this->assertSuccess( $result );
-	}
-
-	public function testValidLogRequest() {
-		list( $result,, ) = $this->doApiRequestWithToken( [
-			'action' => 'thank',
-			'log' => $this->logId,
-		] );
-		$this->assertSuccess( $result );
-	}
-
-	public function testLogRequestWithDisallowedLogType() {
-		// Empty the log-type whitelist.
-		$this->setMwGlobals( [ 'wgThanksLogTypeWhitelist' => [] ] );
-		$this->setExpectedException(
-			ApiUsageException::class,
-			"Log type 'delete' is not in the whitelist of permitted log types."
-		);
-		$this->doApiRequestWithToken( [
-			'action' => 'thank',
-			'log' => $this->logId,
-		] );
-	}
-
-	public function testLogThanksForADeletedLogEntry() {
-		global $wgUser;
-
-		// Mark our test log entry as deleted.
-		// To do this we briefly switch back to our 'uploader' test user.
-		$this->doLogin( 'uploader' );
-		$wgUser->mRights[] = 'deletelogentry';
-		$this->doApiRequestWithToken( [
-			'action' => 'revisiondelete',
-			'type'   => 'logging',
-			'ids'    => $this->logId,
-			'hide'   => 'content',
-		] );
-		$this->doLogin( 'sysop' );
-
-		// Then try to thank for it, and we should get an exception.
-		$this->setExpectedException(
-			ApiUsageException::class,
-			"The requested log entry has been deleted and thanks cannot be given for it."
-		);
-		$this->doApiRequestWithToken( [
-			'action' => 'thank',
-			'log' => $this->logId,
-		] );
 	}
 
 	public function testValidRequestWithSource() {
