b7cd517fb6a0fc1d83e30abaf83a211a5e0b4349
Rafid Aslam <rafidteam@gmail.com>
2018-11-18 22:55:46 +0700

    Move all apihelp translatable messages to separate files
    Should be merged along with
    Ie566f9b0b6ba267470c01f17c3f4ea74482e2d2d
    
    Bug: T189982
    Change-Id: Id3d44fe0e7884276f7c81253f6b765984f5c9945

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@
 
 /* eslint-env node, es6 */
 module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
@@ -20,9 +21,7 @@ module.exports = function ( grunt ) {
 				'tests/qunit/**/*.js'
 			]
 		},
-		banana: {
-			all: 'i18n/'
-		},
+		banana: conf.MessagesDirs,
 		watch: {
 			files: [
 				'.eslintrc.json',
