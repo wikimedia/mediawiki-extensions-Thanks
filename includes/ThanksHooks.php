4d18a1e093d02fe0a0f56950260bbb8e3ec111fb
DannyS712 <DannyS712.enwiki@gmail.com>
2020-06-30 07:31:23 +0000

    BeforeSpecialMobileDiffDisplay hook: Only accept RevisionRecord
    Compatibility was added so that MobileFrontend could switch from
    Revision to RevisionRecord, but now that the switch has occurred no
    need to keep checking to see if a Revision is being passed
    
    See [1] for the MobileFrontend change that required compatibility
    
    [1] https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/MobileFrontend/+/e42c52527b7c8083e6395bee7a7981394fb0db4e
    
    Bug: T246284
    Change-Id: Ie320454a0e90d7f98004a3b776010aff784bbf4d

--- includes/ThanksHooks.php
+++ includes/ThanksHooks.php
@@ -266,14 +266,11 @@ class ThanksHooks {
 	 * Add thanks button to SpecialMobileDiff page
 	 * @param OutputPage &$output OutputPage object
 	 * @param MobileContext $ctx MobileContext object
-	 * @param array $revisions Array of the two revisions that are being compared in the diff
+	 * @param array $revisions Array with two elements, either nulls or RevisionRecord objects for
+	 *     the two revisions that are being compared in the diff
 	 */
 	public static function onBeforeSpecialMobileDiffDisplay( &$output, $ctx, $revisions ) {
 		$rev = $revisions[1];
-
-		if ( $rev && $rev instanceof Revision ) {
-			$rev = $rev->getRevisionRecord();
-		}
 
 		// If the MobileFrontend extension is installed and the user is
 		// logged in or recipient is not a bot if bots cannot receive thanks, show a 'Thank' link.
