c777bb97818a84ded6fa4b5c6c7faa6bdad6f6c9
Umherirrender <umherirrender_de.wp@web.de>
2026-01-03 02:47:10 +0100

    tests: Handle missing CheckUserInsert service without CheckUser
    Define the service before setting it, to handle tests without CheckUser
    
    Follow-Up: I4758cd971a4e8f3bebdefd642a1d79df4476120a
    Change-Id: Ie43783d736f8b21817156ef273231b78b8f04cb5

--- tests/phpunit/integration/Storage/LogStoreTest.php
+++ tests/phpunit/integration/Storage/LogStoreTest.php
@@ -72,6 +72,11 @@ class LogStoreTest extends MediaWikiIntegrationTestCase {
 				->with( 'CheckUser' )
 				->willReturn( false );
 
+			$serviceContainer = $this->getServiceContainer();
+			if ( !$serviceContainer->hasService( 'CheckUserInsert' ) ) {
+				// define as no-op and override afterwards to use MediaWikiIntegrationTestCase service reset
+				$serviceContainer->defineService( 'CheckUserInsert', static fn () => null );
+			}
 			$this->setService(
 				'CheckUserInsert',
 				fn () => $this->fail( 'The CheckUserInsert service was expected to not be called' )
